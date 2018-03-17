24265f946f23b29bea41d239dfa14f480b666fc2
ceashley <colbyeashley@gmail.com>
2018-03-17 17:22:14 -0600

    added a ship list for the ship command and error checking

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,3 @@
-
 import sys
 import irc.bot
 import requests
@@ -8,6 +7,7 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         self.client_id = client_id
         self.token = token
         self.channel = '#' + channel
+        self.shipList = list()
 
         # Get the channel id, we will need this for v5 API calls
         url = 'https://api.twitch.tv/kraken/users?login=' + channel
@@ -20,7 +20,13 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         port = 6667
         print ('Connecting to ' + server + ' on port ' + str(port) + '...')
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port, 'oauth:'+token)], username, username)
-        
+
+        #read in the ship list and lower case it
+        with open('shiplist.txt') as f:
+            self.shipList = f.read().split("\n")
+        f.close
+        self.shipList = [x.lower() for x in self.shipList]
+            
 
     def on_welcome(self, c, e):
         print ('Joining ' + self.channel)
@@ -44,12 +50,21 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         c = self.connection
         commandList = cmd.split()
         command = commandList[0]
-        
+
+
         if command == "test":
             print("current command: {} ".format(commandList))
-        elif command == "ship":
-            message = "https://wiki.eveuniversity.org/" + commandList[1]
-            c.privmsg(self.channel, message)
+
+        elif command == "ship":            
+            shipList = self.shipList
+            ship = commandList[1].lower()
+            if ship in shipList:
+                message = "https://wiki.eveuniversity.org/" + ship
+                c.privmsg(self.channel, message)
+            else:
+                message = ship + " is not a ship"
+                c.privmsg(self.channel, message)
+            print("processed command ship for " + ship)
         # The command was not recognized
         else:
             print("Did not understand command: " + cmd)
@@ -59,10 +74,10 @@ def main():
         print("Usage: twitchbot <username> <client id> <token> <channel>")
         sys.exit(1)
 
-    username  = sys.argv[1]
+    username = sys.argv[1]
     client_id = sys.argv[2]
-    token     = sys.argv[3]
-    channel   = sys.argv[4]
+    token = sys.argv[3]
+    channel = sys.argv[4]
 
     bot = TwitchBot(username, client_id, token, channel)
     bot.start()
